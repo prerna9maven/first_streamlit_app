1e9fdbda749e888cb56fb4d595955d64109ae358
prerna9maven <111425704+prerna9maven@users.noreply.github.com>
2022-11-21 11:37:41 +0530

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,2 +1,6 @@
 import streamlit
 streamlit.title("My first streamlit file")
+streamlit.header("first header")
+streamlit.text("first text line")
+streamlit.text("second text line")
+streamlit.text("third text line")
