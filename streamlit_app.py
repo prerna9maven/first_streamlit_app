37c31391593faa8fd9c77cb2224c394985416832
prerna9maven <111425704+prerna9maven@users.noreply.github.com>
2022-11-23 14:43:20 +0530

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,5 +1,5 @@
 import streamlit
-#import pandas
+import pandas
 import snowflake.connector
 from urllib.error import URLError
 
@@ -30,7 +30,7 @@ streamlit.header("Fruityvice Fruit Advice!")
 fruit_choice = streamlit.text_input('What fruit would you like information about?','Kiwi')
 streamlit.write('The user entered ', fruit_choice)
 
-#import requests
+import requests
 fruityvice_response = requests.get("https://fruityvice.com/api/fruit/" + "kiwi")
 #  streamlit.text(fruityvice_response.json())
 
