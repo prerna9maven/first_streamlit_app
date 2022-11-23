232d955027eb92f5dc3ba0e42a84f0a642c6c455
prerna9maven <111425704+prerna9maven@users.noreply.github.com>
2022-11-23 15:16:06 +0530

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -60,18 +60,17 @@ except URLError as e:
 
 
 
-'''
 # adding snowflake connector
-my_cnx = snowflake.connector.connect(**streamlit.secrets["snowflake"])
-my_cur = my_cnx.cursor()
+# my_cnx = snowflake.connector.connect(**streamlit.secrets["snowflake"])
+# my_cur = my_cnx.cursor()
 # my_cur.execute("SELECT CURRENT_USER(), CURRENT_ACCOUNT(), CURRENT_REGION()")
-my_cur.execute("SELECT * from fruit_load_list")
+# my_cur.execute("SELECT * from fruit_load_list")
 
 #getting all the rows, not just one
 #my_data_row = my_cur.fetchone()
-my_data_rows = my_cur.fetchall()
-streamlit.dataframe(my_data_rows)
-'''
+# my_data_rows = my_cur.fetchall()
+# streamlit.dataframe(my_data_rows)
+
 
 # streamlit.text("Hello from Snowflake:")
 #making the display a little better
