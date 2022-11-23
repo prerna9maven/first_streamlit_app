a8890199bedafc0c23438a89dfaaf22cfb874094
prerna9maven <111425704+prerna9maven@users.noreply.github.com>
2022-11-23 14:34:58 +0530

    Update streamlit_app.py
    #adding execute statement to check control of flow

--- streamlit_app.py
+++ streamlit_app.py
@@ -60,3 +60,5 @@ streamlit.dataframe(my_data_rows)
 add_my_fruit = streamlit.text_input('What fruit would you like to add?','Jackfruit')
 streamlit.write('Thanks for adding ', add_my_fruit)
 
+#adding execute statement to check control of flow
+my_cur.execute("insert into fruit_load_list values ('from streamlit') ")
