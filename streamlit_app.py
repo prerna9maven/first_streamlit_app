23fa53b13b78fc99ee51f0a30e35045dbbde8d12
prerna9maven <111425704+prerna9maven@users.noreply.github.com>
2022-11-23 14:56:04 +0530

    Update streamlit_app.py
    introduce try except box

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,5 +1,6 @@
 import streamlit
 import pandas
+import requests
 import snowflake.connector
 from urllib.error import URLError
 
@@ -26,19 +27,30 @@ streamlit.dataframe(fruits_to_show)
 
 # New section to display fruityvice API response
 streamlit.header("Fruityvice Fruit Advice!")
-# adding a text box to enter fruit choice
-fruit_choice = streamlit.text_input('What fruit would you like information about?','Kiwi')
-streamlit.write('The user entered ', fruit_choice)
+try:
+  # adding a text box to enter fruit choice
+  fruit_choice = streamlit.text_input('What fruit would you like information about?')
+  if not fruit_choice:
+    streamlit.error("Please select a fruit to get information")
+  else:
+    fruityvice_response = requests.get("https://fruityvice.com/api/fruit/" + fruit_choice)
+    # takes the json version and normalize it 
+    fruityvice_normalized = pandas.json_normalize(fruityvice_response.json())
+    # output it on screen as a table
+    streamlit.dataframe(fruityvice_normalized)
 
-import requests
-fruityvice_response = requests.get("https://fruityvice.com/api/fruit/" + "kiwi")
+except URLError as e:
+  streamlit.error()
+
+  #streamlit.write('The user entered ', fruit_choice)
+
+
+
 #  streamlit.text(fruityvice_response.json())
 
 
-# takes the json version and normalize it 
-fruityvice_normalized = pandas.json_normalize(fruityvice_response.json())
-# output it on screen as a table
-streamlit.dataframe(fruityvice_normalized)
+
+
 
 #adding stop statement to check control flow
 streamlit.stop()
